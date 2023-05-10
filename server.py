fcacd39ee55d48d84ecf4ede73f34af8b4f79728
Glitch (purrfect-tranquil-option) <none>
2023-05-10 08:17:16 +0000

    Import of anjani084/portfolio

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for, request
+import csv
+app = Flask(__name__,template_folder='templates', static_folder='static')
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return render_template('thankyou.html')
+        except:
+            return 'Did not saved to Database'
+    else:
+        return 'something went wrong. Try again!'
+
+@app.after_request
+def set_content_type(response):
+    response.headers['Content-Type'] = 'text/html'
+    return response
